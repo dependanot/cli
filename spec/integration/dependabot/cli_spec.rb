7ece878e305cf4d1d8c99b8971eec389956cd32f
mo khan <mo@mokhan.ca>
2021-12-22 10:36:41 -0700

    test: add integration test for bundler v2

--- spec/integration/dependabot/cli_spec.rb
+++ spec/integration/dependabot/cli_spec.rb
@@ -2,21 +2,29 @@
 
 RSpec.describe Dependabot::CLI, type: :cli do
   it "executes `dependabot help scan` command successfully" do
-    output = `./exe/dependabot help scan`
-    expect(output).to eq(fixture_file_content("help-scan.expected"))
+    expect(`./exe/dependabot help scan`).to eq(fixture_file_content("help-scan.expected"))
   end
 
   describe Dependabot::CLI::Scan do
+    subject!(:dependabot) { File.join(Dir.pwd, "exe/dependabot") }
+
     context "when scanning a bundler v1 project" do
       it "publishes a pull request for each package" do
-        root_dir = Dir.pwd
-        Dir.mktmpdir do |path|
-          Dir.chdir(path) do
-            system "git clone https://github.com/dependanot/dependalot ."
-            system "gem install 'bundler:~>1.0'"
-            system "#{root_dir}/exe/dependabot scan #{path}/src/bundler/v1/"
-            expect(`git branch | grep dependanot | wc -l`.chomp.to_i).to be > 35
-          end
+        within_tmp_dir do |path|
+          system "git clone https://github.com/dependanot/dependalot ."
+          system "gem install 'bundler:~>1.0'"
+          system "#{dependabot} scan #{path}/src/bundler/v1/"
+          expect(`git branch | grep dependanot | wc -l`.chomp.to_i).to be > 35
+        end
+      end
+    end
+
+    context "when scanning a bundler v2 project" do
+      it "publishes a pull request for each package" do
+        within_tmp_dir do |path|
+          system "git clone https://github.com/dependanot/dependalot ."
+          system "#{dependabot} scan #{path}/src/bundler/v2/"
+          expect(`git branch | grep dependanot | wc -l`.chomp.to_i).to be > 40
         end
       end
     end
