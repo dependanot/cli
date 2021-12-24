382d21a014f28c4b53c3eb1133dfd5898da577a5
mo khan <mo@mokhan.ca>
2021-12-24 14:34:27 -0700

    test: add tests for #checkout

--- spec/integration/dependabot/git_spec.rb
+++ spec/integration/dependabot/git_spec.rb
@@ -1,27 +1,51 @@
 # frozen_string_literal: true
 
 RSpec.describe ::Dependabot::Git do
-  describe "#commit" do
-    context "when a tracked file is changed" do
-      def setup_git_repo(path)
-        system "git init --quiet #{path}"
-        system "git config user.email you@example.com"
-        system "git config user.name example"
-        system "echo 'hello' > README.md"
-        system "git add README.md"
-        system "git commit --quiet --no-gpg-sign -m 'initial commit'"
-        system "echo 'change' > README.md"
+  def setup_git_repo(path)
+    system "git init --quiet #{path}"
+    system "git config user.email you@example.com"
+    system "git config user.name example"
+    system "echo 'hello' > README.md"
+    system "git add README.md"
+    system "git commit --quiet --no-gpg-sign -m 'initial commit'"
+    system "echo 'change' > README.md"
+  end
+
+  describe "#checkout" do
+    context "when the branch does not exist" do
+      let(:branch_name) { "example" }
+
+      it "creates a new branch" do
+        within_tmp_dir do |path|
+          setup_git_repo(path)
+          subject = described_class.new(path)
+          subject.checkout(branch: branch_name)
+
+          expect(`git branch`).to include(branch_name)
+        end
       end
 
+      it "switches to the new branch" do
+        within_tmp_dir do |path|
+          setup_git_repo(path)
+          subject = described_class.new(path)
+          subject.checkout(branch: branch_name)
+
+          expect(File.read(".git/HEAD").chomp).to eql("ref: refs/heads/#{branch_name}")
+        end
+      end
+    end
+  end
+
+  describe "#commit" do
+    context "when a tracked file is changed" do
       def within_dir
-        Dir.mktmpdir("dependabot") do |path|
-          Dir.chdir(path) do
-            setup_git_repo(path)
-            subject = described_class.new(path)
-            subject.checkout(branch: "example")
-            subject.commit(all: true, message: "The message")
-            yield
-          end
+        within_tmp_dir do |path|
+          setup_git_repo(path)
+          subject = described_class.new(path)
+          subject.checkout(branch: "example")
+          subject.commit(all: true, message: "The message")
+          yield
         end
       end
 
